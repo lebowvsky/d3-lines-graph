a4f28b42d3b1555c0f3aeca66a493558a7baecc8
Brice <lebowvsky@gmail.com>
2022-02-20 17:04:27 +0100

    📈  add graphs

---
+++ index.js
@@ -0,0 +1,43 @@
+// DOM
+const btns = document.querySelectorAll("button");
+const form = document.querySelector("form");
+const formAct = document.querySelector("form span");
+const input = document.querySelector("input");
+const error = document.querySelector(".error");
+
+let activity = "cycling";
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    activity = e.target.dataset.activity;
+    // remove and add active class
+    btns.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    // set id of input field
+    input.setAttribute("id", activity);
+
+    //set text of form span
+    formAct.textContent = activity;
+  });
+});
+
+// form submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const distance = parseInt(input.value);
+  if (distance) {
+    db.collection("activities")
+      .add({
+        distance,
+        activity,
+        date: new Date().toDateString(),
+      })
+      .then(() => {
+        error.textContent = "";
+        input.value = "";
+      });
+  } else {
+    error.textContent = "Please enter a valid distance";
+  }
+});
